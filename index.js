2201e82f2396bceb2e57b0ff9268e9b802c5526d
Derek Joseph <kered.christy@gmail.com>
2019-04-11 12:08:27 +0530

    added port

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+const PORT = process.env.PORT || 3000
+
 app.use(express.static(path.join(__dirname + "/public/")));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/ball-blast.html");
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
